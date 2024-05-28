f9a7de028250459cb0315f6f7ce876297cb43dc2
developerkarthick <developerkarthick191@gmail.com>
2024-05-28 22:36:23 +0530

    Updated webpack default config

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  devtool: "inline-source-map",
+  devServer: {
+    port: 3000,
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.(css)$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpeg|jpg)$/,
+        type: "asset/resource"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html"
+    })
+  ],
+  resolve: {
+    extensions: [".js",".jsx"]
+  }
+}
