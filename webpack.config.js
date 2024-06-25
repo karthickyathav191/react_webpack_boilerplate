1a8d66a70a85455a21cdc800ff0ccf2bf2c1995f
developerkarthick <developerkarthick191@gmail.com>
2024-06-25 10:25:29 +0530

    webpack config file formatted with prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   devServer: {
     port: 3000,
     historyApiFallback: true
@@ -11,25 +11,25 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: "babel-loader"
+        loader: 'babel-loader'
       },
       {
         test: /\.(css)$/,
-        use: ["style-loader", "css-loader"]
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.(png|svg|jpeg|jpg)$/,
-        type: "asset/resource"
+        type: 'asset/resource'
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
-      filename: "index.html"
+      template: './public/index.html',
+      filename: 'index.html'
     })
   ],
   resolve: {
-    extensions: [".js",".jsx"]
+    extensions: ['.js', '.jsx']
   }
 }
